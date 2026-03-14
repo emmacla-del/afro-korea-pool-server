3d4d1ab5015d42f3311ff51ff4584fdd5d73ecfb
emmacla-del <tmbuminyuy@gmail.com>
2026-03-14 20:49:38 +0000

    Add UserCheckIn migration

---
+++ prisma/migrations/20260314204443_add_user_checkin/migration.sql
@@ -0,0 +1,14 @@
+﻿-- CreateTable
+CREATE TABLE "UserCheckIn" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "checkInDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "reward" INTEGER,
+    CONSTRAINT "UserCheckIn_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserCheckIn_userId_checkInDate_key" ON "UserCheckIn"("userId", "checkInDate");
+
+-- AddForeignKey
+ALTER TABLE "UserCheckIn" ADD CONSTRAINT "UserCheckIn_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
